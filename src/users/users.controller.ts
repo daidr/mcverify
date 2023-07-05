9b026f07b4c2e7e8b63f424e755738e11ebeaf83
daidr <daidr@daidr.me>
2023-07-06 03:21:33 +0800

    feat: api route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -38,12 +38,20 @@ export class UsersController {
     }
   }
 
-  @Get('/api/bind/mojang/:uuid')
+  @Get('/api/get/mojang/:uuid')
   async bindMojang(@Param('uuid') uuid: string) {
-    const user = await this.usersService.findOneByUuidMojang(uuid);
+    const _uuid = formatUuid(uuid);
+    if (!_uuid) {
+      return {
+        code: -1,
+        msg: 'invalid uuid',
+      };
+    }
+    const user = await this.usersService.findOneByUuidMojang(_uuid);
     if (user) {
       return {
         code: 0,
+        msg: 'ok',
         data: {
           binded: true,
         },
@@ -51,6 +59,7 @@ export class UsersController {
     } else {
       return {
         code: 0,
+        msg: 'ok',
         data: {
           binded: false,
         },
@@ -58,3 +67,17 @@ export class UsersController {
     }
   }
 }
+
+const formatUuid = (uuid: string) => {
+  // 如果不是uuid，返回 false
+  if (uuid.length === 36) {
+    return uuid;
+  } else if (uuid.length === 32) {
+    return `${uuid.slice(0, 8)}-${uuid.slice(8, 12)}-${uuid.slice(
+      12,
+      16,
+    )}-${uuid.slice(16, 20)}-${uuid.slice(20)}`;
+  } else {
+    return false;
+  }
+};
